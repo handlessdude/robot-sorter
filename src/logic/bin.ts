b046fd929666bd6461f3444b06a91429720a8497
Anoth <mixaetel@gmail.com>
2022-05-24 21:24:22 +0300

    added interfaces to @/types, added imports

--- src/logic/bin.ts
+++
@@ -1,9 +0,0 @@
-class Bin {
-  coordinates: Point;
-  itemType: number;
-
-  constructor(coordinates: Point, itemType: number) {
-    this.coordinates = coordinates;
-    this.itemType = itemType;
-  }
-}
