cb77d2bf12a2eb5378e51d1f486a640fb10e1f37
Anoth <mixaetel@gmail.com>
2022-05-26 17:38:18 +0300

    added funcs for a bearing rotating and a drive moving

--- src/logic/manipulator.ts
+++ src/logic/manipulator.ts
@@ -1,34 +1,49 @@
 import type { IItem } from "@/types/itemTypes";
 import type { IPoint } from "@/types/point";
 import type { Bin } from "@/logic/bin";
-import { distance } from "@/utils/utils";
+import {
+  distance,
+  isOnTraffic,
+  getMovedPoint,
+  toStandartRadianForm,
+} from "@/utils/utils";
 import { worldConstants } from "@/stupidConstants/worldConstants";
 import { hri } from "human-readable-ids";
+
+const WPI = 2 * Math.PI;
+
 export class Manipulator {
   readonly id: string;
   readonly radius: number;
   readonly coordinates: IPoint;
+  bins: Array<Bin>;
+  _currentBearingAngle: number;
+  _currentDrivePlaceScale: number;
+  holdedItem?: IItem;
+
+  bearingTarget?: number;
+  driveTarget?: number;
+
+  // shiza
   readonly size_radius: number;
   color: string;
   strokeColor: string;
-  bins: Array<Bin>;
-  _currentBearingAngle: number;
-  _currentDrivePlace: number;
-  holdedItem?: IItem;
 
   set currentBearingAngle(value: number) {
     if (value >= 0 && value <= 2 * Math.PI) this._currentBearingAngle = value;
     else console.warn("The angle is in [0; 2PI]");
   }
 
-  set currentDrivePlace(value: number) {
-    if (value >= 0 && value <= 1) this._currentDrivePlace = value;
-    else console.warn("The angle is in [0; 1]");
+  set currentDrivePlaceScale(value: number) {
+    if (value >= 0 && value <= 1) this._currentDrivePlaceScale = value;
+    else console.warn("The drive scale is in [0; 1]");
   }
 
   constructor(
     radius: number,
     coordinates: IPoint,
+
+    // shiza
     size_radius = 25,
     color = worldConstants.MANIP_COLORS.FILL_COLOR,
     strokeColor = worldConstants.MANIP_COLORS.STROKE_COLOR
@@ -37,21 +52,30 @@ export class Manipulator {
     this.radius = radius;
     this.coordinates = coordinates;
     this.bins = <Bin[]>[];
-    this._currentBearingAngle = 0;
-    this._currentDrivePlace = 0;
+    this._currentBearingAngle = Math.PI;
+    this._currentDrivePlaceScale = 0.5;
     this.holdedItem = undefined;
+
+    this.bearingTarget = undefined;
+    this.driveTarget = undefined;
+
+    // shiza
     this.size_radius = size_radius;
     this.color = color;
     this.strokeColor = strokeColor;
   }
 
+  getDriveCoordinates(): IPoint {
+    return getMovedPoint(
+      this.coordinates,
+      this.radius * this.currentDrivePlaceScale,
+      this.currentBearingAngle
+    );
+  }
+
   findBins(bins: Array<Bin>): void {
     //манипуляторам придётся самим найти свои урны
-    // for (const bin of inputState.$state.data.bins) {
-    //   if (distance(bin.coordinates, this.coordinates) <= this.radius) {
-    //     this.bins.push(bin);
-    //   }
-    // }
+    // bins: inputState.$state.data.bins
     for (const bin of bins) {
       if (distance(bin.coordinates, this.coordinates) <= this.radius) {
         this.bins.push(bin);
@@ -74,41 +98,123 @@ export class Manipulator {
     }
   }
 
-  throwItem(): void {
-    if (this.holdedItem == undefined) return;
+  tryThrowItem(): void | boolean {
+    if (this.holdedItem == undefined) return false;
 
     this.holdedItem.holdedBy = undefined;
     // BINS MUST BE SPLITTED
     for (const bin of this.bins) {
       if (
-        distance(bin.coordinates, this.holdedItem!.coordinates) <=
-        worldConstants.GRAB_DISTANCE
+        distance(bin.coordinates, this.holdedItem.coordinates) <=
+        worldConstants.THROWING_DISTANCE
       ) {
-        if (bin.itemType == this.holdedItem!.item_type) {
+        if (bin.itemType == this.holdedItem.item_type) {
           bin.itemsPlaced++;
           // TODO ! delete this.holdedItem
           this.holdedItem = undefined;
+          return true;
+        }
+      }
+    }
+
+    if (isOnTraffic(this.getDriveCoordinates())) {
+      this.holdedItem.holdedBy = undefined;
+      this.holdedItem = undefined;
+    }
+
+    return false;
+  }
+
+  // Поставить задачу поворота подшипника таким образом, чтобы радианы совпадали с указанными
+  ST_rotateBearing(radians: number): void {
+    this.bearingTarget = toStandartRadianForm(radians);
+  }
+
+  // Возвращает время, необходимое для выполнения
+  bearingRotatingTime(radians: number, bearingVelocity: number): number {
+    return (
+      Math.abs(this.currentBearingAngle - toStandartRadianForm(radians)) /
+      bearingVelocity
+    );
+  }
+
+  // повернуть подшипник
+  rotateBearing(bearingVelocity: number): void {
+    if (this.bearingTarget == undefined) return;
+
+    let a = this.currentBearingAngle;
+    let b = this.bearingTarget;
+    let toUp = true;
+    if (a > b) {
+      [a, b] = [b, a];
+      toUp = false;
+    }
+    if (Math.abs(b - a) < Math.abs(a + WPI - b)) {
+      if (toUp) {
+        if (bearingVelocity > this.bearingTarget - this.currentBearingAngle)
+          this.currentBearingAngle += bearingVelocity;
+        else {
+          this.currentBearingAngle = this.bearingTarget;
+          this.bearingTarget = undefined;
+        }
+      } else {
+        if (bearingVelocity > this.currentBearingAngle - this.bearingTarget)
+          this.currentBearingAngle -= bearingVelocity;
+        else {
+          this.currentBearingAngle = this.bearingTarget;
+          this.bearingTarget = undefined;
         }
       }
     }
   }
 
-  rotateBearing(radians: number): void {
-    // Повернуть подшипник таким образом, чтобы радианы совпадали с указанными
+  // Поставить задачу передвижения привода таким образом, чтобы местно совпадало с указанным
+  ST_moveDrive(placeScale: number): void {
+    if (placeScale < 0 || placeScale > 1) return;
+    this.driveTarget = placeScale;
   }
 
-  bearingRotating(radians: number): number {
-    // Возвращает время, необходимое для выполнения
-    return 0;
+  // Возвращает время, необходимое для выполнения
+  driveMovingTime(placeScale: number, driveVelocity: number): number {
+    if (placeScale < 0 || placeScale > 1) return -1;
+    return (
+      (this.radius * Math.abs(this.currentDrivePlaceScale - placeScale)) /
+      driveVelocity
+    );
   }
 
-  moveDrive(place: number): void {
-    // Передвинуть привод таким образом, чтобы местно совпадало с указанным
+  // передвинуть привод
+  moveDrive(driveVelocity: number): void {
+    if (this.driveTarget == undefined) return;
+
+    if (this.currentDrivePlaceScale < this.driveTarget) {
+      if (this.driveTarget - this.currentDrivePlaceScale < driveVelocity)
+        this.currentDrivePlaceScale += driveVelocity;
+      else {
+        this.currentDrivePlaceScale = this.driveTarget;
+        this.driveTarget = undefined;
+      }
+    } else {
+      if (this.currentDrivePlaceScale - this.driveTarget < driveVelocity)
+        this.currentDrivePlaceScale -= driveVelocity;
+      else {
+        this.currentDrivePlaceScale = this.driveTarget;
+        this.driveTarget = undefined;
+      }
+    }
   }
 
-  driveMoving(place: number): number {
-    // Возвращает время, необходимое для выполнения
-    return 0;
+  // Запускать в каждом кадре для изменения параметров
+  update(bearingVelocity: number, driveVelocity: number): void {
+    // thinking
+    //TODO:
+    // choosing an action
+    //TODO:
+    // moving
+    this.rotateBearing(bearingVelocity);
+    this.moveDrive(driveVelocity);
+    // acting
+    //TODO:
   }
 
   draw(ctx: CanvasRenderingContext2D) {
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,5 @@
 import type { IPoint } from "@/types/point";
+import { worldConstants } from "@/stupidConstants/worldConstants";
 
 export function lerp(A: number, B: number, t: number) {
   return A + (B - A) * t;
@@ -25,4 +26,28 @@ export function isGreaterThan(num: number, threshold: number) {
 
 export function isPositive(num: number) {
   return isGreaterThan(num, 0);
+}
+
+export function isOnTraffic(coordinates: IPoint): boolean {
+  return (
+    coordinates.x >= worldConstants.ITEM_WIDTH &&
+    coordinates.x <= worldConstants.LINE_WIDTH - worldConstants.ITEM_WIDTH
+  );
+}
+
+export function getMovedPoint(
+  point: IPoint,
+  radius: number,
+  angle: number
+): IPoint {
+  return {
+    x: radius * Math.cos(angle),
+    y: radius * Math.sin(angle),
+  };
+}
+
+export function toStandartRadianForm(angle: number): number {
+  angle = angle % (2 * Math.PI);
+  if (angle < 0) angle += 2 * Math.PI;
+  return angle;
 }
